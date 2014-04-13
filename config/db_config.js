f23eda2c9b9871e21ff3bf593194a301ffba939a
Johan Wanglof <jwanglof@gmail.com>
2014-04-13 22:32:13 +0100

    Added an option for zeCrib-NAS

--- config/db_config.js
+++ config/db_config.js
@@ -1,4 +1,4 @@
-var config = "local";
+var config = "zecrib";
 
 exports.dbConfig = function() {
     var dbInformation;
@@ -7,6 +7,8 @@ exports.dbConfig = function() {
 		dbInformation = {host: "localhost", user: "root", password: "geanbe33", database: "mealz2"};
 	else if (config == "jumpstarter")
 		dbInformation = {host: "127.0.0.1", user: "root", password: "", database: "mealz"};
+    else if (config == "zecrib")
+        dbInformation = {host: "10.0.0.10", user: "root", password: "geanbe33", database: "mealz2"};
     
 	return dbInformation;
 }
