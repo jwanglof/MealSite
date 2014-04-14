55ae80c434426826fe16ba0ca4af10fec53c524e
Johan Wanglof <jwanglof@gmail.com>
2014-04-14 16:47:36 +0100

    Nothing

--- config/db_config.js
+++ config/db_config.js
@@ -1,14 +1,16 @@
-var config = "zecrib";
+var config = "local";
 
 exports.dbConfig = function() {
     var dbInformation;
 
-	if (config == "local")
+	if ( config == "local" )
 		dbInformation = {host: "localhost", user: "root", password: "geanbe33", database: "mealz2"};
-	else if (config == "jumpstarter")
+	else if ( config == "jumpstarter" )
 		dbInformation = {host: "127.0.0.1", user: "root", password: "", database: "mealz"};
-    else if (config == "zecrib")
-        dbInformation = {host: "10.0.0.10", user: "root", password: "geanbe33", database: "mealz2"};
+  else if ( config == "zecrib" )
+    dbInformation = {host: "10.0.0.10", user: "root", password: "geanbe33", database: "mealz2"};
+  else if ( config == "zecrib-extern" )
+    dbInformation = {host: "79.136.91.46", user: "root", password: "geanbe33", database: "mealz2"};
     
 	return dbInformation;
 }
